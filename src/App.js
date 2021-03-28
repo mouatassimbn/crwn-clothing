553b7e6f19f2551fe773127c6db9d298a477a249
mouatassim boulahsen <m.ouatassim89@gmail.com>
2021-03-28 17:15:23 -0300

    Installed and added sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import HomePage from "./hompage.component";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
